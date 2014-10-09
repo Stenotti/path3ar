75370f2850c9c3357cbbe3e259e787f7f8000a17
Stefano Tombolini <st.tombolini@gmail.com>
2014-10-09 15:32:04 +0200

    asd

--- web/getSamples.php
+++ web/getSamples.php
@@ -3,25 +3,16 @@
 $conn_string = "host=pathdb.ccokugosuzr8.eu-west-1.rds.amazonaws.com port=5432 dbname=pathdb user=pathadmin password=pathdbpwd";
 $dbconn = pg_connect($conn_string) or die('connection failed');
 
-/*mysqli_select_db($con,"ajax_demo");
-$sql="SELECT * FROM sample";
-$result = mysqli_query($con,$sql);
+$result = pg_exec($dbconn, "select id, latitude, longitude from sample");
+$numrows = pg_numrows($result);
 
-echo "<table border='1'>
-<tr>
-<th>ID</th>
-<th>Latitude</th>
-<th>Longitude</th>ì
-</tr>";
-
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['id'] . "</td>";
-  echo "<td>" . $row['latitude'] . "</td>";
-  echo "<td>" . $row['longitude'] . "</td>";
-  echo "</tr>";
+$response = array();
+for($ri = 0; $ri < $numrows; $ri++) {
+    $row = pg_fetch_row($result, $ri);
+	$response[] = $row;
 }
-echo "</table>";
 
-mysqli_close($con);*/
+$jsonData = json_encode($response); 
+echo $jsonData;
+pg_close($dbconn);
 ?>
