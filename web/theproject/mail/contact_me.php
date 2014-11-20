55cb22938caa891b51de9e5a6b64057ec1b381b7
Stenotti <st.tombolini@gmail.com>
2014-11-20 15:39:48 +0100

    asd

--- web/theproject/mail/contact_me.php
+++ web/theproject/mail/contact_me.php
@@ -13,6 +13,7 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
+header("content-type: application/javascript");
 // Create the email and send the message
 $to = 'st.tombolini@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
