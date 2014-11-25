38b62e2a73f79568607ccdd0b26f2d174a149180
Stenotti <st.tombolini@gmail.com>
2014-11-25 17:44:03 +0100

    asd

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
     <link href="css/index.css" rel="stylesheet" type="text/css">
     <link href="css/atooltip.css" rel="stylesheet" type="text/css">
     <link href="css/ui.switchbutton.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
 	
 	<script src="js/customFormat.js"></script>
 	<script src="js/jquery.min.js"></script>
@@ -27,9 +27,7 @@
 		
 		var samplesNoiseTimedData = [numberOfTimeRanges]; // Lista dei valori di rumorosità dei campionamenti divisi per l'orario (ogni quarto d'ora) -> per la mappa
 		var samplesTimedDataGraph = [numberOfTimeRanges]; // Lista dei valori di rumorosità dei campionamenti divisi per l'orario (ogni quarto d'ora) -> per il grafico
-		
 		var samplesLightTimedData = [numberOfTimeRanges]; // Lista dei valori di lumonosità dei campionamenti divisi per l'orario (ogni quarto d'ora -> 24*4) -> per la mappa
-		//var samplesLightTimedDataGraph = [numberOfTimeRanges]; // Lista dei valori di lumonosità dei campionamenti divisi per l'orario (ogni quarto d'ora -> 24*4) -> per il grafico
 		
 		var currentArrayIndex;
 		
@@ -42,6 +40,9 @@
 		var currentShown = -1; // 0 = Samples, 1 = Light, 2 = Noise
 		var hours = "00", minutes = "00";
 		var offsetMS;
+		
+		var mindate = -1;
+		var maxdate = -1;
 		
 		var lightGraph, noiseGraph;
 		
@@ -73,6 +74,10 @@
 						
 						var ms=Date.parse(jsonCoord[3])-offsetMS;
 						var date = new Date(ms);
+						if(mindate == -1 || mindate > ms)
+							mindate=ms;
+						if(maxdate == -1 || maxdate < ms)
+							maxdate=ms;
 						var hourtime = date.customFormat("#DD#/#MM#/#YYYY# #hhh#:#mm#:#ss#");
 						var name = "LIGHT: "+light+", NOISE: "+noise+" "+hourtime;
 						var coord = new google.maps.LatLng(jsonCoord[1], jsonCoord[2]);
@@ -137,7 +142,9 @@
 					var radius = document.getElementById('radiusM').value;
 					var dist = distanceInMeters(currentCoord.lat(), currentCoord.lng(), jsonCoord[1], jsonCoord[2]);
 					if(dist <= radius){
-						graphData.push(jsonCoord); // Inserisco il sample nell'array graphData
+						var ms=Date.parse(jsonCoord[3])-offsetMS;
+						if(ms >= mindate && ms <= maxdate)
+							graphData.push(jsonCoord); // Inserisco il sample nell'array graphData
 					}
 				}
 				if(graphData.length != 0){
@@ -155,7 +162,6 @@
 							plotXLines.push(ms);
 							lastDate = hourtime;
 						}
-						
 						var light = getLightFromJson(label);
 						var noise = getNoiseFromJson(label);
 						var toAddLight = [];
@@ -187,26 +193,14 @@
 							value: plotXLines[i],
 							width: 1,
 							color: 'green',
-							dashStyle: 'dash'/*,
-							label: {
-								text: datetext,
-								align: 'center',
-								y: 12,
-								x: 0
-							}*/
+							dashStyle: 'dash'
 						});
 						noiseGraph.xAxis[0].addPlotLine({
 							id: 'noise-plotLine',
 							value: plotXLines[i],
 							width: 1,
 							color: 'green',
-							dashStyle: 'dash'/*,
-							label: {
-								text: datetext,
-								align: 'center',
-								y: 12,
-								x: 0
-							}*/
+							dashStyle: 'dash'
 						});
 					}
 				}
@@ -235,7 +229,57 @@
 			createTimedArrays();
 			map.fitBounds(bounds);
 			
-			$('#time').html("00:00 - 00:"+(sliderStep-1));
+			$( "#datepickerfrom" ).datepicker({
+				dateFormat: 'dd/mm/yy',
+				onSelect: function(dateText, inst) {
+					removeMarker();
+					var datefrom = $(this).val();
+					var msdate = new Date();
+					msdate.setFullYear(Number(datefrom.split("/")[2]), Number(datefrom.split("/")[1]-1), Number(datefrom.split("/")[0]));
+					msdate.setHours(0);
+					msdate.setMinutes(0);
+					msdate.setSeconds(0);
+					msdate.setMilliseconds(0);
+					var tmpmindate=msdate.getTime();
+					if(tmpmindate > maxdate){
+						alert("Empty range");
+						$( "#datepickerfrom" ).datepicker("setDate", new Date(mindate) );
+					}
+					else mindate = tmpmindate;
+					if(currentShown == 0) toggleSamples();
+					else if(currentShown == 1) toggleLight();
+					else if(currentShown == 2) toggleNoise();
+				}
+			});
+			$( "#datepickerto" ).datepicker({
+				dateFormat: 'dd/mm/yy',	
+				onSelect: function(dateText, inst) {
+					removeMarker();
+					var dateto = $(this).val();
+					var msdate = new Date();
+					msdate.setHours(23);
+					msdate.setMinutes(59);
+					msdate.setSeconds(59);
+					msdate.setMilliseconds(999);
+					msdate.setFullYear(Number(dateto.split("/")[2]), Number(dateto.split("/")[1]-1), Number(dateto.split("/")[0]));
+					var tmpmaxdate=msdate.getTime();
+					if(tmpmaxdate < mindate){
+						alert("Empty range");
+						$( "#datepickerto" ).datepicker("setDate", new Date(maxdate) );
+					}
+					else maxdate = tmpmaxdate;
+					if(currentShown == 0) toggleSamples();
+					else if(currentShown == 1) toggleLight();
+					else if(currentShown == 2) toggleNoise();
+				}
+			});
+			$( "#datepickerfrom" ).datepicker("setDate", new Date(mindate) );
+			$( "#datepickerto" ).datepicker("setDate", new Date(maxdate) );
+			
+			if(sliderStep<=60)
+				$('#time').html("00:00 - 00:"+(sliderStep-1));
+			else
+				$('#time').html("00:00 - "+parseInt((sliderStep-1)/60)+":59");
 			$('#numSamples').html("Number of samples: 0");
 			
 			var width_but1 = $("#samplesButton").width();
@@ -376,6 +420,29 @@
 				onShow: null,                   // callback function that fires after atooltip has shown  
 				onHide: null                    // callback function that fires after atooltip has faded out      
 			});
+			$('#fromto').aToolTip({  
+				// no need to change/override 
+				toolTipId: 'aToolTip',  
+				// ok to override  
+				fixed: false,                   // Set true to activate fixed position  
+				clickIt: false,                 // set to true for click activated tooltip  
+				inSpeed: 200,                   // Speed tooltip fades in  
+				outSpeed: 100,                  // Speed tooltip fades out  
+				tipContent: '',                 // Pass in content or it will use objects 'title' attribute  
+				toolTipClass: 'defaultTheme',   // Set class name for custom theme/styles  
+				xOffset: 5,                     // x position  
+				yOffset: 5,                     // y position  
+				onShow: null,                   // callback function that fires after atooltip has shown  
+				onHide: null                    // callback function that fires after atooltip has faded out      
+			});
+			$( "#timestep" ).change(function() {
+				sliderStep =  parseInt($(this).val());
+				createTimedArrays();
+				$('#slider-time').slider('option', 'step', sliderStep);
+				$('#slider-time').slider('option', 'max', 1440-sliderStep);
+				$('#slider-time').slider('value',0);
+				slideToggleFunction(0);
+			});
 		}
 		
 		function clickFunction(coord){
@@ -420,7 +487,14 @@
 			if(hours < 10) hours = '0' + hours;
 			if(minutes.length < 10) minutes = '0' + minutes;
 			if(minutes == 0) minutes = '00';
-			$('#time').html(hours+':'+minutes+" - "+hours+':'+(parseInt(minutes)+sliderStep-1));
+			
+			var hourend = parseInt(hours)+parseInt((sliderStep-1)/60);
+			var minend = parseInt(minutes)+parseInt((sliderStep-1)%60);
+			if(hourend < 10) hourend = '0' + hourend;
+			if(minend.length < 10) minend = '0' + minend;
+			if(minend == 0) minend = '00';
+			
+			$('#time').html(hours+':'+minutes+" - "+hourend+':'+minend);
 			
 			if(currentShown == 1 || currentShown == 2){
 				updateDataInTimeRange();
@@ -471,11 +545,15 @@
 		}
 		
 		function createTimedArrays(){
+			numberOfTimeRanges = 1440 / sliderStep;
+			samplesIndexes = [numberOfTimeRanges];
+			samplesNoiseTimedData = [numberOfTimeRanges];
+			samplesTimedDataGraph = [numberOfTimeRanges];
+			samplesLightTimedData = [numberOfTimeRanges];
 			for(var i=0; i<numberOfTimeRanges; i++) {
 				samplesLightTimedData[i] = [];
 				samplesTimedDataGraph[i] = [];
 				samplesNoiseTimedData[i] = [];
-				//samplesNoiseTimedDataGraph[i] = [];
 				samplesIndexes[i] = [];
 			}
 			for(var i=0; i<samplesData.length; i++){
@@ -485,7 +563,7 @@
 				var hourtime = date.customFormat("#hhh#:#mm#:#ss#");
 				var sampleHour = hourtime.split(":")[0];
 				var sampleMins = hourtime.split(":")[1];
-				var arrayIndex = sampleHour*(60/sliderStep) + parseInt(sampleMins/sliderStep);
+				var arrayIndex = parseInt(sampleHour*(60/sliderStep) + parseInt(sampleMins/sliderStep));
 				
 				var coord = new google.maps.LatLng(jsonCoord[1], jsonCoord[2]);
 				var light = getLightFromJson(jsonCoord);
@@ -514,6 +592,9 @@
 					samplesNoiseTimedData[arrayIndex].push({location:coord, weight: noise/10});
 				}
 			}
+			/*for(var i=0; i<numberOfTimeRanges; i++){
+				console.log("#"+i+": "+samplesLightTimedData[i].length);
+			}*/
 			
 		}
 		
@@ -522,15 +603,28 @@
 				heatmap.setData([]);
 				setAllMap(null);
 				currentArrayIndex = parseInt(hours)*(60/sliderStep) + parseInt(minutes/sliderStep);
+				
+				var lightDataTimeRange = [];
+				var noiseDataTimeRange = [];
 				for(var i=0; i<samplesIndexes[currentArrayIndex].length; i++){
-					setMarkerMap(samplesIndexes[currentArrayIndex][i], map);
+					
+					var jsonCoord = samplesData[samplesIndexes[currentArrayIndex][i]];
+					var ms=Date.parse(jsonCoord[3])-offsetMS;
+					if(ms >= mindate && ms <= maxdate){
+						var coord = new google.maps.LatLng(jsonCoord[1], jsonCoord[2]);
+						setMarkerMap(samplesIndexes[currentArrayIndex][i],map);
+						if(currentShown == 1)
+							lightDataTimeRange.push(samplesLightTimedData[currentArrayIndex][i]);
+						else if(currentShown == 2)
+							noiseDataTimeRange.push(samplesNoiseTimedData[currentArrayIndex][i]);
+					}
 				}
 				var dataToSet;
 				if(currentShown == 1){
-					dataToSet = new google.maps.MVCArray(samplesLightTimedData[currentArrayIndex]);
+					dataToSet = new google.maps.MVCArray(lightDataTimeRange);
 				}
 				else if(currentShown == 2){
-					dataToSet = new google.maps.MVCArray(samplesNoiseTimedData[currentArrayIndex]);
+					dataToSet = new google.maps.MVCArray(noiseDataTimeRange);
 				}
 				heatmap.setData(dataToSet);
 			}
@@ -541,15 +635,28 @@
 			$("#lightButton").removeClass("buttonActive");
 			$("#noiseButton").removeClass("buttonActive");
 			document.getElementById("bottomSlider").style.display = "none";
-			if(currentShown != 0){
+			//if(currentShown != 0){
 				currentShown = 0;
 				heatmap.setData([]);
-				setAllMap(map);
-				var pointArray = new google.maps.MVCArray(samplesCoordsData);
+				setAllMap(null);
+				var samplesCoordsDataTimeRange = [];
+				for (var i = 0; i < samplesData.length; i++) {
+					var jsonCoord = samplesData[i]; 
+					//[0]=id, [1]=lat, [2]=lng, [3]=timestamp, [4]=type1, [5]=value1, [6]=type2, [7]=value2
+					var ms=Date.parse(jsonCoord[3])-offsetMS;
+					if(ms >= mindate && ms <= maxdate){
+						var coord = new google.maps.LatLng(jsonCoord[1], jsonCoord[2]);
+						setMarkerMap(i,map);
+						samplesCoordsDataTimeRange.push(coord);
+					}
+				}
+				if(samplesCoordsDataTimeRange.length == 0)
+					alert("There are no samples in this range");
+				var pointArray = new google.maps.MVCArray(samplesCoordsDataTimeRange);
 				heatmap.setData(pointArray);
 				if(currentCoord != null) showGraph();
 				if(isPlaying) playpause();
-			}
+			//}
 		}
 		
 		function toggleLight() {
@@ -558,14 +665,14 @@
 			$("#noiseButton").removeClass("buttonActive");
 			//removeMarker();
 			document.getElementById("bottomSlider").style.display = "block";
-			if(currentShown != 1){
+			//if(currentShown != 1){
 				currentShown = 1;
 				heatmap.setData([]);
 				setAllMap(null);
 				updateDataInTimeRange();
 				if(currentCoord != null) showGraph();
 				if(isPlaying) playpause();
-			}
+			//}
 		}
 		
 		function toggleNoise() {
@@ -574,14 +681,14 @@
 			$("#noiseButton").addClass("buttonActive");
 			//removeMarker();
 			document.getElementById("bottomSlider").style.display = "block";
-			if(currentShown != 2){
+			//if(currentShown != 2){
 				currentShown = 2;
 				heatmap.setData([]);
 				setAllMap(null);
 				updateDataInTimeRange();
 				if(currentCoord != null) showGraph();
 				if(isPlaying) playpause();
-			}
+			//}
 		}
 		
 		function removeMarker() {
@@ -606,7 +713,7 @@
 				document.getElementById("contentGraphs").style.display = "table-cell";
 				document.getElementById("contentMap").width = "50%";
 				document.getElementById("controlButtons").style.right = "51%";
-				document.getElementById("panel").style.right = "51%";
+				document.getElementById("removeMarkerId").style.right = "51%";
 				document.getElementById("bottomSlider").style.right = "55%";
 				document.getElementById("bottomSlider").style.right = "55%";
 				document.getElementById("image_fullscren").src="img/fullscreen_in.png";
@@ -615,7 +722,7 @@
 				document.getElementById("contentGraphs").style.display = "none";
 				document.getElementById("contentMap").width = "100%";
 				document.getElementById("controlButtons").style.right = "2%";
-				document.getElementById("panel").style.right = "2%";
+				document.getElementById("removeMarkerId").style.right = "2%";
 				document.getElementById("bottomSlider").style.right = "13%";
 				document.getElementById("image_fullscren").src="img/fullscreen_out.png";
 			}
@@ -713,10 +820,10 @@
   </head>
 
   <body>
-    <div id="panel" class="labeltextbox">
-	  <div id="infoRadius"  title="Set the marker radius">Radius: <input type="number" id="radiusM" max="30000" min="1" size="2" value="100" class="textbox"> m</div><br><br>
-      <button onclick="removeMarker()" id="removeMarkerId" class="myButton" style="display:none"  title="Remove the marker from the clicked position">Remove marker<img src="img/marker.png" height="23px" alt="Remove Marker" style="padding-left: 5px; vertical-align:middle;"/></button>
-    </div>
+	<button onclick="removeMarker()" id="removeMarkerId" class="myButton" style="display:none"  title="Remove the marker from the clicked position"><font size=3>Remove marker</font><img src="img/marker.png" height="23px" alt="Remove Marker" style="padding-left: 5px; vertical-align:middle;"/></button>
+    
+	<div id="fromto" class="labeltextbox" title="Set the dates range"><b>From:<input type="text" size="8" id="datepickerfrom">	To:<input type="text" size="8" id="datepickerto"></b></div></div>
+	
 	<div id="bottomPanel">
       <button onclick="toggleSamples()" id="samplesButton" title="Location of samples" class="myButton"><font size=5>Samples</font></button><br>
       <button onclick="toggleLight()" id="lightButton" title="Light level, green means low light while red means high light" class="myButton"><font size=5>Light</font></button><br>
@@ -726,12 +833,25 @@
 	<div id="bottomSlider" >
 		<table width="100%" height="100%">
 			<tr>
-				<td width="25%">
+				<td width="30%">
 					<center><input type="checkbox" id="fastslow" style="position: relative;" /></center>
 				</td>
-				<td width="75%">
+				<td width="40%">
 					<center><b>Time range: <label id="time"></label></b></center>
 				</td>
+				<td width="30%">
+					<center>
+						<b>Time step</b>
+						<select id="timestep" class="styled-select blue semi-square">
+						  <option value=15>15 min</option>
+						  <option value=30>30 min</option>
+						  <option value=60 selected="selected">60 min</option>
+						  <option value=120>120 min</option>
+						  <option value=180>180 min</option>
+						</select>
+					</center>
+				</td>
+				
 			</tr>
 		</table>
 		<table width="100%" height="100%">
@@ -758,11 +878,12 @@
 				<div id="map-canvas" ></div>
 			</td>
 			<td width="50%" id="contentGraphs" >
+				<div id="infoRadius" class="labeltextbox" title="Set the marker radius">Radius: <input type="number" id="radiusM" max="30000" min="1" size="2" value="100" class="textbox"> m</div>
 				<div class="scrollable">
 					<center><b><div id="numSamples"></div></b></center>
-					<button class="myButton" id="graphLightToggle">Show/Hide Light Graph</button>
+					<button class="myButton" id="graphLightToggle" style="float: right;">Show/Hide Light Graph</button>
 					<div id="graphLight"></div><br>
-					<button class="myButton" id="graphNoiseToggle">Show/Hide Noise Graph</button>
+					<button class="myButton" id="graphNoiseToggle" style="float: right;">Show/Hide Noise Graph</button>
 					<div id="graphNoise"></div>
 				</div>
 			</td>
